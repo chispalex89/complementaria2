cbb1328b661102570b92007b86ec82b7dac577fb
Laboratorio de Software T-213 <t213@laburl>
2019-08-31 10:33:40 -0600

    read/write

--- Complementaria2/Program.cs
+++ Complementaria2/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.IO;
 
 namespace Complementaria2
 {
     class Program
     {
+        const int bufferLength = 1000;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using(var stream = new FileStream("C:\\Users\\t213\\Desktop\\prueba.txt", FileMode.Open))
+            {
+                using (var reader = new BinaryReader(stream))
+                {
+                    using(var writeStream = new FileStream("C:\\Users\\t213\\Desktop\\prueba2.txt", FileMode.OpenOrCreate))
+                    {
+                        using (var writer = new BinaryWriter(writeStream))
+                        {
+                            var byteBuffer = new byte[bufferLength];
+                            while (reader.BaseStream.Position != reader.BaseStream.Length)
+                            {
+                                byteBuffer = reader.ReadBytes(bufferLength);
+                                writer.Write(byteBuffer);
+                            }
+                        }
+                    }   
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
